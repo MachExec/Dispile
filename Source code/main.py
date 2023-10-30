f60784404fed2b938700a4f6986b28bca109acc4
MachExec <max591b@gmail.com>
2023-10-29 19:56:25 -0700

    get contents of discord, some minor testing

--- Source code/main.py
+++ Source code/main.py
@@ -1,16 +1,47 @@
 import requests
+import time
 
 class dispile:
-    def __init__(self,c,t)->None:
+    def __init__(self,channel:str,token:str)->None:
         
-        self.channel = c
-        self.token = t
-        self.link = f"https://discord.com/api/v9/channels/{self.channel}/messages"
+        self.channel:str = channel
+        self.token:str = token
 
-    def compile(self):
-        headers = {"authorization": self.token} 
+        self.post_link:str = f"https://discord.com/api/v9/channels/{self.channel}/messages"
+        self.get_link:str = f"{self.post_link}?limit=50"
+
+        self.headers:dict = {"authorization": self.token} 
+        self.start_time:float = time.time()
+
+
+
+
+    def get_message(self)->None:
+        
+        message_content = requests.get(self.get_link,headers=self.headers)
+        json_content = message_content.json()
+        isolate_message = {}
+
+        for index,item in enumerate(json_content):
+            message = item.get("content")
+            time = item.get("timestamp")
+            isolate_message.append(message)
+
+            if message == "53643tbnfjfefefefenhr":
+                print("true")
+
+        return isolate_message
+    
+
+
+    def compile(self)->str:
+        
         message = "Hello"
         data = {"content": message}
-        requests.post(self.link,headers=headers,json=data)
+        requests.post(self.post_link,headers=self.headers,json=data)
 
+        return message
 
+test_instance = dispile(channel="1167715294047899648",token="")
+while(True):
+    liust = test_instance.get_message()
